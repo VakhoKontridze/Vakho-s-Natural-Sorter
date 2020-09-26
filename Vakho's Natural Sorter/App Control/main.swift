431dc34b6b95fe1e70d8033b561c868ca26822ae
Vaktang Kontridze <vakho.kontridze@gmail.com>
2020-09-26 13:14:17 +0400

    Add app menu

--- Vakho's Natural Sorter/App Control/main.swift
+++ Vakho's Natural Sorter/App Control/main.swift
@@ -9,7 +9,7 @@ import Cocoa
 let appDelegate: AppDelegate = .init()
 NSApplication.shared.delegate = appDelegate
 
-//private let menuBar: NSMenu = AppMenu(settings: appDelegate.settings)
-//NSApplication.shared.mainMenu = menuBar
+private let menuBar: NSMenu = AppMenu(settings: appDelegate.settings)
+NSApplication.shared.mainMenu = menuBar
 
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
