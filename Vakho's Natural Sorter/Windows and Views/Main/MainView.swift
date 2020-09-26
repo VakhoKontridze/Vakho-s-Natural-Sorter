e44ad4cb56f35c94dd49dcad3e9f81364b1bc2bf
Vaktang Kontridze <vakho.kontridze@gmail.com>
2020-09-26 11:37:35 +0400

    Add Natural Comparison algorithm

--- Vakho's Natural Sorter/Windows and Views/Main/MainView.swift
+++ Vakho's Natural Sorter/Windows and Views/Main/MainView.swift
@@ -71,7 +71,7 @@ extension MainView {
             Spacer()
             
             Button("Sort", action: {
-                print("SORT")
+                text = NaturalComparator.sort(text)
             })
             
             Spacer()
